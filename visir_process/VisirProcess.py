4ca4655b32804343aad7b4db09d5ed2acc54760c
Padraig Donnelly <padraig2.pd@gmail.com>
2022-06-10 18:33:33 +0200

    Update VisirProcess.py

--- visir_process/VisirProcess.py
+++ visir_process/VisirProcess.py
@@ -25,6 +25,7 @@ def main():
     calc        = 0                                   # (0) Calculate meridional profiles, (1) read stored profiles
     save        = 0                                   # (0) Do not save (1) save meridional profiles
     plot        = 0                                   # (0) Do not plot (1) plot meridional profiles
+    maps        = 0                                   # (0) Do not plot (1) plot cylindrical maps
     spx         = 0                                   # (0) Do not write (1) do write spxfiles for NEMESIS input
     
     if calc == 0:
@@ -59,7 +60,7 @@ def main():
             PlotProfiles(singles=profiles1, spectrals=profiles2, ksingles=coeffs1, kspectrals=coeffs2)
 
     # Plot cylindrical maps (optionally read stored numpy arrays from Step 8)
-    if plot == 1:
+    if maps == 1:
         if calc == 0:
             PlotMaps(files, ksingles, kspectrals)
         if calc == 1:
