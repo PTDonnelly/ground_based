26ba1a2e77f8dc4cb7aa64a5236351feb9c97478
Padraig Donnelly <padraig2.pd@gmail.com>
2022-06-10 15:36:51 +0200

    Added better errors

--- visir_process/CalculateErrors.py
+++ visir_process/CalculateErrors.py
@@ -1,7 +1,36 @@
-def CalculateErrors(image):
-    """Carry out some experiments on the measurement error on the radiance values"""
+import numpy as np
+import matplotlib.pyplot as plt
 
-    # Currently set to a typical reasonable value
-    nominal_error = 0.05
+def CalculateErrors(image, view):
+    """Calculate radiance error from the variability background sky flux"""
+
+    # # Set to a constant typical value
+    # error = 0.05
+
+    # Only use unaffected hemisphere
+    ny, nx = np.shape(image)
+    if view == 1:
+        y1, y2 = int(ny/2), -1
+        img = image[y1:y2, :]
+    if view == -1:
+        y1, y2 = 0, int(ny/2)-1
+        img = image[y1:y2, :]
+    # Set a radiance threshold to separate "planet" from "sky" flux
+    thresh = 0.25
+    # Calculate mean radiance value of the image
+    mean_flux = np.mean(img)
+    # Isolate sky
+    keep  = (img < thresh * mean_flux)
+    sky   = img[keep]
+    error = np.std(sky)
+    # print(f"STDDEV: {error}")
+
+    # sky  = img[np.ix_(keep[0], keep[1])]
+    # plt.figure
+    # ax = plt.subplot2grid((1, 2), (0, 0))
+    # ax.imshow(img, origin="lower")
+    # ax = plt.subplot2grid((1, 2), (0, 1))
+    # ax.imshow(sky, origin="lower")
+    # plt.show()
     
-    return nominal_error
+    return error
