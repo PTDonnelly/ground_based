652f2b5c98666039834a07c06c63818c0ffffe2c
Deborah Bardet <43414461+debbardet@users.noreply.github.com>
2022-06-10 18:40:44 +0100

    Edited (first) version of PlotProfiles for plotting single and spectral merid profiles calibration

--- visir_process/PlotProfiles.py
+++ visir_process/PlotProfiles.py
@@ -1,20 +1,50 @@
 def PlotProfiles(singles, spectrals, ksingles, kspectrals):
-    a = 1
-    print('Plotting')
+    import matplotlib.pyplot as plt
+    from ReadCal import ReadCal
+    from BinningInputs import BinningInputs
+    from VisirWavenumbers import VisirWavenumbers
+    from SetWave import SetWave
+    
+    # Read in Voyager and Cassini data into arrays
+    calfile = "../visir.jup.filtered-iris-cirs.10-12-15.data.v3"
+    iris, cirs = ReadCal(calfile)
 
-    # plt.figure(dpi=900)
-    # ax1 = plt.subplot2grid((2, 1), (0, 0))
-    # ax1.plot(lat_grid, calib, color='k', lw=1, label='calib_full')
-    # ax1.plot(lat_grid[calibkeep], calibdata, color='k', lw=3, label='calib_keep')
-    # ax1.plot(lat_grid[visirkeep], visirdata, color='skyblue', lw=0, marker='o', markersize=3, label='visir_raw')
-    # ax1.plot(lat_grid[visirkeep], visirdata/(visirmean/calibmean), color='orange', lw=0, marker='o', markersize=3, label='visir_calib')
-    # ax1.set_xlim((-90, 90))
-    # ax1.set_ylim((0, 20e-8))
-    # ax1.legend()
-    # ax2 = plt.subplot2grid((2, 1), (1, 0))
-    # ax2.plot(lat_grid[singlekeep], singledata, color='black', lw=0, marker='.', markersize=5, label='single')
-    # ax2.plot(lat_grid[spectralkeep], spectraldata, color='red', lw=2, label='spectral')
-    # ax2.set_xlim((-90, 90))
-    # ax2.set_ylim((0, 20e-8))
-    # ax2.legend()
-    # plt.savefig('figure.png', dpi=900)
+    print('Plotting profiles')
+    dir = 'calibration_profiles_figures/'
+    for ifilt in range(BinningInputs.nfilters):
+        # Get filter index for plotting spacecraft and calibrated data
+        waves = spectrals[:, ifilt, 5]
+        wave  = waves[(waves > 0)][0]
+        _, _, ifilt_sc, ifilt_v = SetWave(wavelength=False, wavenumber=wave)
+        # Create a figure per filter
+        #plt.figure(dpi=900)
+        for ifile, wave in enumerate(waves):
+            # Get filter index for spectral profiles
+            _, _, ifilt_sc, ifilt_v = SetWave(wavelength=False, wavenumber=wave)
+            # subplot showing the averaging of each singles merid profiles (ignoring negative beam)
+            ax1 = plt.subplot2grid((2, 1), (1, 0))
+            #ax1.plot(singles[:, ifile, 0], singles[:, ifile, 3], lw=0, marker='.', markersize=5, label='single')
+            ax1.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3]*kspectrals[ifilt_v, 1], color='orange', lw=0, marker='o', markersize=3, label='visir_av')
+            ax1.set_xlim((-90, 90))
+            ax1.set_ylim((0, 20e-8))
+            ax1.legend()
+            # subplot showing the calibration of the spectral merid profile to spacecraft data
+            ax2 = plt.subplot2grid((2, 1), (0, 0))
+            if ifilt_sc < 12:
+                # Use CIRS for N-Band
+                ax2.plot(cirs[:, ifilt_sc, 0], cirs[:, ifilt_sc, 1], color='k', lw=1, label='Cassini/CIRS')
+            else:
+                # Use IRIS for Q-Band
+                ax2.plot(iris[:, ifilt_sc, 0], iris[:, ifilt_sc, 1], color='k', lw=1, label='Voyager/IRIS')
+            ax2.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3], color='skyblue', lw=0, marker='o', markersize=3, label='visir_raw')
+            ax2.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3]/kspectrals[ifilt_v, 1], color='orange', lw=0, marker='o', markersize=3, label='visir_calib')
+            ax2.set_xlim((-90, 90))
+            ax2.set_ylim((0, 20e-8))
+            ax2.legend()
+        # Save figure showing calibation method 
+        filt = VisirWavenumbers(ifilt)
+        plt.savefig(f"{dir}{filt}_calibration_merid_profiles.png", dpi=900)
+        plt.savefig(f"{dir}{filt}_calibration_merid_profiles.eps", dpi=900)
+
+
+
