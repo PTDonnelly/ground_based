5a94e95f89f024323c0ccd45cd6260e4d78872f3
Bardet <db496@host-49-146.eduroam-local.wifi.le.ac.uk>
2022-06-10 15:46:29 +0100

    Add calibration coefficient writing for single merid and spectal merid profiles

--- visir_process/WriteProfiles.py
+++ visir_process/WriteProfiles.py
@@ -18,10 +18,15 @@ def WriteProfiles(files, singles, spectrals, ksingles, kspectrals):
         name = name[0].split('/')
         # Write individual mean profiles to np.array
         np.save(f"{dir}{name[-1]}_merid_profile", singles[:, ifile, :])
+        np.save(f"{dir}{name[-1]}_calib_coeff", ksingles[ifile, :])
         # Write individual mean profiles to textfile
         np.savetxt(f"{dir}{name[-1]}_merid_profile.txt", singles[:, ifile, :],
                     fmt=['%4.2f', '%5.2f', '%4.2f', '%8.5e', '%8.5e', '%8.5f', '%s'],
                     header='LAT    LCM    MU    RAD    ERROR    NU    VIEW')
+        np.savetxt(f"{dir}{name[-1]}_calib_coeff.txt", ksingles[ifile, :],
+                    fmt=['%8.5f'],
+                    header='FILE INDEX        CALIB_COEFF')
+        
     
     # If subdirectory does not exist, create it
     dir = 'spectral_merid_profiles/'
@@ -29,10 +34,14 @@ def WriteProfiles(files, singles, spectrals, ksingles, kspectrals):
         os.makedirs(dir)
     # Save spectral meridional profiles
     for ifilt in range(BinningInputs.nfilters):
-        # Write individual mean profiles to np.array
+        # Write spectral mean profiles and calibrated coefficient to np.array
         filt = VisirWavenumbers(ifilt)
         np.save(f"{dir}{filt}_merid_profile", spectrals[:, ifilt, :])
-        # Write individual mean profiles to textfile
+        np.save(f"{dir}{filt}_calib_coeff", kspectrals[ifilt, :])
+        # Write spectral mean profiles and calibrated coefficient to textfiles
         np.savetxt(f"{dir}{filt}_merid_profile.txt", spectrals[:, ifilt, :],
                     fmt=['%4.2f', '%5.2f', '%4.2f', '%8.5e', '%8.5e', '%8.5f'],
                     header='LAT    LCM    MU    RAD    ERROR    NU')
+        np.savetxt(f"{dir}{filt}_calib_coeff.txt", kspectrals[ifilt, :],
+                    fmt=['%8.5f'],
+                    header='NU      CALIB_COEFF')
