e3c767a43eb34fe6db28ec62aa8a138af7f36a0c
Padraig Donnelly <padraig2.pd@gmail.com>
2022-06-14 15:39:42 +0200

    Updated WriteSpx.py

--- visir_process/WriteSpx.py
+++ visir_process/WriteSpx.py
@@ -6,6 +6,36 @@ from BinningInputs import BinningInputs
 def WriteSpx(spectrals):
     """Create spectral input for NEMESIS. Take calculated profiles and 
     populate .spxfile with radiances, measurement errors, and geometries."""
+    
+    def create_merid_spx(f, lats, LCMs, mus, rads, rad_errs, waves):
+        # Loop over NGEOM geometries (no. of geometries = no. of emission angle points)
+        for igeom, mu in enumerate(mus):
+            # Calculate NCONV spectral points (NCONV = no. of wavenumbers at each geometry = 1 for merid binning)
+            nconv = 1 #len(waves[igeom])
+            # I can't remember what NAV is... check the NEMESIS manual
+            nav   = 1
+            # The "angles line": this defines a "geometry" that holds a "spectrum"
+            clat           = lats[igeom]
+            clon           = LCMs[igeom]
+            solar_ang      = 0
+            emission_angle = mu
+            azimuth_angle  = 0
+            wgeom          = 1 
+            # The spectrum lines: these are the spectral points that occur at a given "geometry"
+            wave    = waves[igeom]
+            rad     = rads[igeom]
+            rad_err = rad_errs[igeom]
+
+            # Write output to texfile with relevant formatting
+            f.write("{0:d}\n".format(nconv))
+            f.write("{0:d}\n".format(nav))
+            f.write("{0:12.5f}  {1:12.5f}  {2:12.5f}  {3:12.5f}  {4:12.5f}  {4:12.5f}\n".format(clat, clon, solar_ang, emission_angle, azimuth_angle, wgeom))
+            f.write("{0:10.4f}  {1:15.6e}  {2:15.6e}\n".format(wave, rad, rad_err))
+
+    # If subdirectory does not exist, create it
+    dir = '../outputs/spxfiles/'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
     print('Creating spectra...')
     # Loop over latitudes to create one .spxfile per latitude
@@ -16,35 +46,12 @@ def WriteSpx(spectrals):
         mus      = [spectrals[ilat, ifilt, 2] for ifilt in range(BinningInputs.nfilters) if np.isnan(spectrals[ilat, ifilt, 2]) == False]
         rads     = [spectrals[ilat, ifilt, 3] for ifilt in range(BinningInputs.nfilters) if np.isnan(spectrals[ilat, ifilt, 3]) == False]
         rad_errs = [spectrals[ilat, ifilt, 4] for ifilt in range(BinningInputs.nfilters) if np.isnan(spectrals[ilat, ifilt, 4]) == False]
-        wavenums = [spectrals[ilat, ifilt, 5] for ifilt in range(BinningInputs.nfilters) if np.isnan(spectrals[ilat, ifilt, 5]) == False]
+        waves = [spectrals[ilat, ifilt, 5] for ifilt in range(BinningInputs.nfilters) if np.isnan(spectrals[ilat, ifilt, 5]) == False]
         # Only write spxfile for latitudes with spectral information
         if lats:
-            # If subdirectory does not exist, create it
-            dir = '../outputs/spxfiles/'
-            if not os.path.exists(dir):
-                os.makedirs(dir)
             # Open textfile
             with open(f"../outputs/spxfiles/lat_{lats[0]}.txt", 'w') as f:
-                # Loop over NGEOM geometries (no. of geometries = no. of emission angle points)
-                for igeom, mu in enumerate(mus):
-                    # Calculate NCONV spectral points (NCONV = no. of wavenumbers at each geometry = 1 for merid binning)
-                    nconv = 1 #len(wavenums[igeom])
-                    # I can't remember what NAV is... check the NEMESIS manual
-                    nav   = 1
-                    # The "angles line": this defines a "geometry" that holds a "spectrum"
-                    clat           = lats[igeom]
-                    clon           = LCMs[igeom]
-                    solar_ang      = 0
-                    emission_angle = mu
-                    azimuth_angle  = 0
-                    wgeom          = 1 
-                    # The spectrum lines: these are the spectral points that occur at a given "geometry"
-                    wave    = wavenums[igeom]
-                    rad     = rads[igeom]
-                    rad_err = rad_errs[igeom]
-
-                    # Write output to texfile with relevant formatting
-                    f.write("{0:d}\n".format(nconv))
-                    f.write("{0:d}\n".format(nav))
-                    f.write("{0:12.5f}  {1:12.5f}  {2:12.5f}  {3:12.5f}  {4:12.5f}  {4:12.5f}\n".format(clat, clon, solar_ang, emission_angle, azimuth_angle, wgeom))
-                    f.write("{0:10.4f}  {1:15.6e}  {2:15.6e}\n".format(wave, rad, rad_err))
+                create_merid_spx(f, lats, LCMs, mus, rads, rad_errs, waves)
+            # Open spxfile
+            with open(f"../outputs/spxfiles/lat_{lats[0]}.spx", 'w') as f:
+                create_merid_spx(f, lats, LCMs, mus, rads, rad_errs, waves)
