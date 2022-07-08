7608ab67f9b087e646953632ba447de4031492c9
Deborah Bardet <43414461+debbardet@users.noreply.github.com>
2022-07-08 18:21:29 +0100

    Update of the way to plot cmerid profiles and remove redundante function in PlotProfiles

--- GiantPipe/Plot/PlotProfiles.py
+++ GiantPipe/Plot/PlotProfiles.py
@@ -7,19 +7,16 @@ from Read.ReadCal import ReadCal
 from Tools.VisirFilterInfo import Wavenumbers
 from Tools.SetWave import SetWave
 
-def PlotMeridProfiles(singles, spectrals, ksingles, kspectrals, wavenumber):
-    """ DB: Plot meridian profiles and spacecraft data to illustrate 
+def PlotMeridProfiles(files, singles, spectrals):
+    """ Plot meridian profiles and spacecraft data to illustrate 
             the calibration method """
-    
-    # If wavenumber=False, we need to retrieve wavenumber from each "singles" file/array
-    #if wavenumber=False:
-    #    print("blablabla, you have to code it!")
+
+    print('Plotting profiles...')
 
     # Read in Voyager and Cassini data into arrays
     calfile = "../inputs/visir.jup.filtered-iris-cirs.10-12-15.data.v3"
     iris, cirs = ReadCal(calfile)
 
-    print('Plotting profiles...')
     # If subdirectory does not exist, create it
     dir = '../outputs/calibration_profiles_figures/'
     if not os.path.exists(dir):
@@ -27,38 +24,37 @@ def PlotMeridProfiles(singles, spectrals, ksingles, kspectrals, wavenumber):
 
     for ifilt in range(Globals.nfilters):
         # Get filter index for plotting spacecraft and calibrated data
-        waves = spectrals[:, ifilt, 5]
-        wave  = waves[(waves > 0)][0]
-
-        # Get filter index for spectral profiles
-        _, _, ifilt_sc, ifilt_v = SetWave(wavelength=False, wavenumber=wave)
+        _, _, wave, ifilt_sc, ifilt_v = SetWave(filename=None, wavelength=None, wavenumber=None, ifilt=ifilt)
+        
         # Create a figure per filter
-        #plt.figure(dpi=900)
+        fig, axes = plt.subplots(2, 1, sharex=True, sharey=True)
         # subplot showing the averaging of each singles merid profiles (ignoring negative beam)
-        ax1 = plt.subplot2grid((2, 1), (0, 0))
-        for ifile, iwave in enumerate(wavenumber):
+        for ifile, fname in enumerate(files):
+            _, _, iwave, _, _ = SetWave(filename=fname, wavelength=None, wavenumber=None, ifilt=None)
             if iwave == wave:
-                ax1.plot(singles[:, ifile, 0], singles[:, ifile, 3]*ksingles[ifile, 1], color='midnightblue', lw=0, marker='.', markersize=3)
-        ax1.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3]*kspectrals[ifilt_v, 1], color='skyblue', lw=0, marker='o', markersize=3, label='VLT/VISIR av')
-        ax1.set_xlim((-90, 90))
-        #ax1.set_ylim((0, 20e-8))
-        ax1.set_ylabel('Radiance (W)', size=15)
-        ax1.legend()
+                axes[0].plot(singles[:, ifile, 0], singles[:, ifile, 3], color='black', lw=0, marker='.', markersize=2)
+        axes[0].plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3], color='orange', lw=0, marker='o', markersize=2, label='VLT/VISIR av')
+        axes[0].set_title(f"{wave}"+" cm$^{-1}$")
+        axes[0].set_xlim((-90, 90))
+        axes[0].legend()
+
         # subplot showing the calibration of the spectral merid profile to spacecraft data
-        ax2 = plt.subplot2grid((2, 1), (1, 0))
         if ifilt_sc < 12:
             # Use CIRS for N-Band
-            ax2.plot(cirs[:, ifilt_sc, 0], cirs[:, ifilt_sc, 1], color='k', lw=1, label='Cassini/CIRS')
+            axes[1].plot(cirs[:, ifilt_sc, 0], cirs[:, ifilt_sc, 1], color='k', lw=1, label='Cassini/CIRS')
         else:
             # Use IRIS for Q-Band
-            ax2.plot(iris[:, ifilt_sc, 0], iris[:, ifilt_sc, 1], color='k', lw=1, label='Voyager/IRIS')
-        ax2.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3], color='orange', lw=0, marker='v', markersize=3, label='VLT/VISIR calib')
-        ax2.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3]*kspectrals[ifilt_v, 1], color='skyblue', lw=0,  marker='o', markersize=3, label='VLT/VISIR av')
-        ax2.set_xlim((-90, 90))
-        ax2.set_xlabel('Latitude', size=15)
-        #ax2.set_ylim((0, 20e-8))
-        ax2.set_ylabel('Radiance (W)', size=15)
-        ax2.legend()
+            axes[1].plot(iris[:, ifilt_sc, 0], iris[:, ifilt_sc, 1], color='k', lw=1, label='Voyager/IRIS')
+        axes[1].plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3], color='orange', lw=0, marker='o', markersize=3, label='VLT/VISIR av')
+        axes[1].set_xlim((-90, 90))
+        axes[1].legend()
+
+        # add a big axis, hide frame
+        fig.add_subplot(111, frameon=False)
+        # hide tick and tick label of the big axis
+        plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
+        plt.xlabel("Latitude", size=15)
+        plt.ylabel("Radiance (W cm$^{-2}$ sr$^{-1}$ (cm$^{-1}$)$^{-1}$)", size=15)
         # Save figure showing calibation method 
         filt = Wavenumbers(ifilt)
         plt.savefig(f"{dir}{filt}_calibration_merid_profiles.png", dpi=900)
@@ -66,62 +62,6 @@ def PlotMeridProfiles(singles, spectrals, ksingles, kspectrals, wavenumber):
     # Clear figure to avoid overlapping between plotting subroutines
     plt.clf()
 
-def PlotCalMeridProfiles(singles, spectrals, wavenumber):
-    """ DB: Plot calibrated meridian profiles and spacecraft data FOR PAPER! """
-    
-    # If wavenumber=False, we need to retrieve wavenumber from each "singles" file/array
-    #if wavenumber=False:
-    #    print("blablabla, you have to code it!")
-
-    # Read in Voyager and Cassini data into arrays
-    calfile = "../inputs/visir.jup.filtered-iris-cirs.10-12-15.data.v3"
-    iris, cirs = ReadCal(calfile)
-
-    print('Plotting profiles...')
-    # If subdirectory does not exist, create it
-    dir = '../outputs/calibration_profiles_figures/'
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-    for ifilt in range(Globals.nfilters):
-        # Get filter index for plotting spacecraft and calibrated data
-        waves = spectrals[:, ifilt, 5]
-        wave  = waves[(waves > 0)][0]
-
-        # Get filter index for spectral profiles
-        _, _, ifilt_sc, ifilt_v = SetWave(wavelength=False, wavenumber=wave)
-        # Create a figure per filter
-        #plt.figure(dpi=900)
-        # subplot showing the averaging of each singles merid profiles (ignoring negative beam)
-        ax1 = plt.subplot2grid((2, 1), (0, 0))
-        for ifile, iwave in enumerate(wavenumber):
-            if iwave == wave:
-                ax1.plot(singles[:, ifile, 0], singles[:, ifile, 3], color='chocolate', lw=0, marker='.', markersize=3)
-        ax1.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3], color='orange', lw=0, marker='o', markersize=3, label='VLT/VISIR')
-        ax1.set_xlim((-90, 90))
-        #ax1.set_ylim((0, 20e-8))
-        ax1.set_ylabel('Radiance (W)', size=15)
-        ax1.legend()
-        # subplot showing the calibration of the spectral merid profile to spacecraft data
-        ax2 = plt.subplot2grid((2, 1), (1, 0))
-        if ifilt_sc < 12:
-            # Use CIRS for N-Band
-            ax2.plot(cirs[:, ifilt_sc, 0], cirs[:, ifilt_sc, 1], color='k', lw=1, label='Cassini/CIRS')
-        else:
-            # Use IRIS for Q-Band
-            ax2.plot(iris[:, ifilt_sc, 0], iris[:, ifilt_sc, 1], color='k', lw=1, label='Voyager/IRIS')
-        ax2.plot(spectrals[:, ifilt_v, 0], spectrals[:, ifilt_v, 3], color='orange', lw=0,  marker='o', markersize=3, label='VLT/VISIR')
-        ax2.set_xlim((-90, 90))
-        ax2.set_xlabel('Latitude', size=15)
-        #ax2.set_ylim((0, 20e-8))
-        ax2.set_ylabel('Radiance (W)', size=15)
-        ax2.legend()
-        # Save figure showing calibation method 
-        filt = Wavenumbers(ifilt)
-        plt.savefig(f"{dir}{filt}_calibrated_merid_profiles.png", dpi=900)
-        plt.savefig(f"{dir}{filt}_calibrated_merid_profiles.eps", dpi=900)
-    # Clear figure to avoid overlapping between plotting subroutines
-    plt.clf()
 
 def ColorNuance(colorm, ncolor, i):
     pal = get_cmap(name=colorm)
