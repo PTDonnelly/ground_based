48262d3979110beb46c7395412ff6098241d309f
Deborah Bardet <43414461+debbardet@users.noreply.github.com>
2022-07-11 16:35:52 +0100

    Adding a flag (adj_location) to determine the latitudal band and method to calculate the plynomial adjustment in CorrectMaps

--- GiantPipe/Tools/CorrectMaps.py
+++ GiantPipe/Tools/CorrectMaps.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt 
 import Globals
 from Read.ReadFits import ReadFits
@@ -12,8 +13,9 @@ def PolynomialAdjust(directory, files, spectrals):
     res    = ny / 180                   # Resolution of maps: res = 1 (1 degree), res = 2 (0.5 degree) etc.
     Nfiles = len(files)
     lat = np.arange(-89.75,90,step=0.5) # Latitude range from pole-to-pole
-    mumin = 0.1
-    
+    mumin_north = [0.15, 0.15, 0.00, 0.5, 0.15, 0.2, 0.5, 0.5, 0.15, 0.3, 0.4, 0.2, 0.05]
+    mumin_south = [0.15, 0.15, 0.00, 0.1, 0.1, 0.2, 0.5, 0.5, 0.15, 0.3, 0.6, 0.5, 0.05]
+
     # Create np.arrays for all pixels in all cmaps and mumaps
     cmaps      = np.empty((Nfiles, ny, nx))
     mumaps     = np.empty((Nfiles, ny, nx))
@@ -32,6 +34,11 @@ def PolynomialAdjust(directory, files, spectrals):
     bandmumaps[:, :, :] = np.nan
     keepdata[:, :, :] = np.nan
     keepmu[:, :, :]   = np.nan
+
+    adj_location = 'northern'    
+    #adj_location = 'southern'
+    #adj_location = 'hemispheric'
+    #adj_location = 'average'
 
     # Loop over file to load individual (and original) cylindrical maps
     for ifile, fpath in enumerate(files):
@@ -99,72 +106,146 @@ def PolynomialAdjust(directory, files, spectrals):
                 for y in range(ny):
                     bandcmaps[ifilt, y, x] = np.nanmean(selectdata[:, y, x])
                     bandmumaps[ifilt, y, x] = np.nanmean(selectmu[:, y, x])
-            # Define a mask depending on minimum emission angle for each hemisphere
-            keep_north  = (lat < 30) & (lat > 5)
-            bandcnorth  = bandcmaps[ifilt, keep_north, :]
-            bandmunorth = bandmumaps[ifilt, keep_north, :]
-            mask_north  = (( bandmunorth > mumin) & (bandcnorth > 90.))
-            keep_south  = (lat < -5) & (lat > -30)
-            bandcsouth  = bandcmaps[ifilt, keep_south, :]
-            bandmusouth = bandmumaps[ifilt, keep_south, :]           
-            mask_south  = ((bandmusouth > mumin) & (bandcsouth > 90.))
-            # Calculate polynomial adjustement for each hemisphere (using mask selections)
-            if (ifilt != 6): # ifilt = 6 is only a southern view
-                p_north = np.poly1d(np.polyfit(bandmunorth[mask_north], bandcnorth[mask_north],4))
-            p_south = np.poly1d(np.polyfit(bandmusouth[mask_south], bandcsouth[mask_south],4))
-            # Define a linear space to show the polynomial adjustment variation over all emission angle range 
-            t = np.linspace(mumin, 0.9, 100)
-            # Some control printing
-            if (ifilt != 6): # ifilt = 6 is only a southern view
-                print('Northern polynome')
-                print(p_north)
-                print(bandcnorth[mask_north])
-            print('Southern polynome')
-            print(p_south)
-            print(bandcsouth[mask_south])            
-            # Correct data on the slected latitude band
-            if (ifilt != 6): # ifilt = 6 is only a southern view
-                cdata_north=bandcnorth[mask_north]*p_north(1)/p_north(bandmunorth[mask_north])
-            cdata_south=bandcsouth[mask_south]*p_south(1)/p_south(bandmusouth[mask_south])    
-            # Plot figure showing limb correction using polynomial adjustment method
-            if (ifilt != 6): # ifilt = 6 is only a southern view
-                ax1 = plt.subplot2grid((2, 3), (0, 0))
-                ax1.scatter(bandmunorth[mask_north], bandcnorth[mask_north])
-                ax1.plot(t, p_north(t), '-',color='red')
-                ax2 = plt.subplot2grid((2, 3), (0, 1))
-                ax2.plot(t, (p_north(1))/p_north(t), '-',color='red')
-                ax3 = plt.subplot2grid((2, 3), (0, 2))
-                ax3.scatter(bandmunorth[mask_north], cdata_north)
+            # Four posibilities to calculate the polynomial adjustemnt
+            # Hemispheric, Northern, Southern, Average:
+            if adj_location == 'hemispheric':
+                # Define a mask depending on minimum emission angle for each hemisphere
+                keep_north  = ((lat < 30) & (lat > 5)) 
+                bandcnorth  = bandcmaps[ifilt, keep_north, :]
+                bandmunorth = bandmumaps[ifilt, keep_north, :]
+                mask_north  = (( bandmunorth > mumin_north[ifilt]) & (bandcnorth > 90.))
+                keep_south  = ((lat < -5) & (lat > -30)) 
+                bandcsouth  = bandcmaps[ifilt, keep_south, :]
+                bandmusouth = bandmumaps[ifilt, keep_south, :]           
+                mask_south  = ((bandmusouth > mumin_south[ifilt]) & (bandcsouth > 90.))
+                # Calculate polynomial adjustement for each hemisphere (using mask selections)
+                if (ifilt != 6): # ifilt = 6 is only a southern view
+                    p_north = np.poly1d(np.polyfit(bandmunorth[mask_north], bandcnorth[mask_north],4))
+                p_south = np.poly1d(np.polyfit(bandmusouth[mask_south], bandcsouth[mask_south],4))
+                # Define a linear space to show the polynomial adjustment variation over all emission angle range
+                mumin = min(mumin_north[ifilt], mumin_south[ifilt]) if ifilt < 10 else mumin_south[ifilt]
+                t_north = np.linspace(mumin_north[ifilt], 0.9, 100)
+                t_south = np.linspace(mumin_south[ifilt], 0.9, 100)
+                # Some control printing 
+                if (ifilt != 6): # ifilt = 6 is only a southern view
+                    print('Northern polynome')
+                    print(p_north)
+                    print(bandcnorth[mask_north])
+                print('Southern polynome')
+                print(p_south)
+                print(bandcsouth[mask_south])            
+                # Correct data on the slected latitude band
+                if (ifilt != 6): # ifilt = 6 is only a southern view
+                    cdata_north=bandcnorth[mask_north]*p_north(1)/p_north(bandmunorth[mask_north])
+                cdata_south=bandcsouth[mask_south]*p_south(1)/p_south(bandmusouth[mask_south])    
+                # Plot figure showing limb correction using polynomial adjustment method
+                if (ifilt != 6): # ifilt = 6 is only a southern view
+                    ax1 = plt.subplot2grid((2, 3), (0, 0))
+                    ax1.scatter(bandmunorth[mask_north], bandcnorth[mask_north])
+                    ax1.plot(t_north, p_north(t_north), '-',color='red')
+                    ax2 = plt.subplot2grid((2, 3), (0, 1))
+                    ax2.plot(t_north, (p_north(1))/p_north(t_north), '-',color='red')
+                    ax3 = plt.subplot2grid((2, 3), (0, 2))
+                    ax3.scatter(bandmunorth[mask_north], cdata_north)
                 ax4 = plt.subplot2grid((2, 3), (1, 0))
-                ax4.scatter(bandmusouth[mask_south], bandcnorth[mask_south])
-                ax4.plot(t, p_south(t), '-',color='red')
+                ax4.scatter(bandmusouth[mask_south], bandcsouth[mask_south])
+                ax4.plot(t_south, p_south(t_south), '-',color='red')
                 ax5 = plt.subplot2grid((2, 3), (1, 1))
-                ax5.plot(t, (p_south(1))/p_south(t), '-',color='red')
+                ax5.plot(t_south, (p_south(1))/p_south(t_south), '-',color='red')
                 ax6 = plt.subplot2grid((2, 3), (1, 2))
                 ax6.scatter(bandmusouth[mask_south], cdata_south)
             else:
-                ax1 = plt.subplot2grid((1, 3), (0, 0))
-                ax1.scatter(bandmusouth[mask_south], bandcsouth[mask_south])
-                ax1.plot(t, p_south(t), '-',color='red')
-                ax2 = plt.subplot2grid((1, 3), (0, 1))
-                ax2.plot(t, (p_south(1))/p_south(t), '-',color='red')
-                ax3 = plt.subplot2grid((1, 3), (0, 2))
-                ax3.scatter(bandmusouth[mask_south], cdata_south)
+                # Define a mask depending on minimum emission angle and the adjustement location chosen
+                if adj_location == 'northern':
+                    if (ifilt != 6): # ifilt = 6 is only a southern view
+                        keep  = ((lat < 30) & (lat > 5))
+                    mumin = mumin_north[ifilt]
+                if adj_location == 'southern':
+                    keep  = ((lat < -5) & (lat > -30)) if (ifilt != 4) else ((lat < -10) & (lat > -15))
+                    mumin = mumin_south[ifilt]
+                bandc  = bandcmaps[ifilt, keep, :]
+                bandmu = bandmumaps[ifilt, keep, :]
+                if adj_location == 'average':
+                    keep_north  = ((lat < 30) & (lat > 5)) 
+                    bandcnorth  = bandcmaps[ifilt, keep_north, :]
+                    bandmunorth = bandmumaps[ifilt, keep_north, :]
+                    mask_north  = (( bandmunorth > mumin_north[ifilt]) & (bandcnorth > 90.))
+                    keep_south  = ((lat < -5) & (lat > -30)) 
+                    bandcsouth  = bandcmaps[ifilt, keep_south, :]
+                    bandmusouth = bandmumaps[ifilt, keep_south, :]           
+                    mask_south  = ((bandmusouth > mumin_south[ifilt]) & (bandcsouth > 90.))
+                    # Average the two hemispheric bands
+                    bandc_combined = np.append(bandcnorth, bandcsouth, axis = 0)
+                    bandmu_combined = np.append(bandmunorth, bandmusouth, axis = 0)
+                    bandc_tmp = np.reshape(bandc_combined, (2, 50, nx))
+                    bandmu_tmp = np.reshape(bandmu_combined, (2, 50, nx))
+                    for x in range(nx):
+                        for y in range(50):
+                            bandc[y, x] = np.nanmean(bandc_tmp[:, y, x])
+                            bandmu[y, x] = np.nanmean(bandmu_tmp[:, y, x])
+                    mumin = min(mumin_north[ifilt], mumin_south[ifilt])
+                mask  = (( bandmu > mumin) & (bandc > 90.))
+                if adj_location == 'northern':
+                    if (ifilt != 6): # ifilt = 6 is only a southern view
+                        # Calculate polynomial adjustement for each hemisphere (using mask selections)
+                        p = np.poly1d(np.polyfit(bandmu[mask], bandc[mask],4))
+                        # Define a linear space to show the polynomial adjustment variation over all emission angle range
+                        t = np.linspace(mumin, 0.9, 100)
+                        # Some control printing
+                        print(f"{adj_location} polynome")
+                        print(p)
+                        print(bandc[mask])
+                        # Correct data on the slected latitude band
+                        cdata=bandc[mask]*p(1)/p(bandmu[mask])
+                        # Plot figure showing limb correction using polynomial adjustment method
+                        ax1 = plt.subplot2grid((1, 3), (0, 0))
+                        ax1.scatter(bandmu[mask], bandc[mask])
+                        ax1.plot(t, p(t), '-',color='red')
+                        ax2 = plt.subplot2grid((1, 3), (0, 1))
+                        ax2.plot(t, (p(1))/p(t), '-',color='red')
+                        ax3 = plt.subplot2grid((1, 3), (0, 2))
+                        ax3.scatter(bandmu[mask], cdata)
+                else:
+                    # Calculate polynomial adjustement for each hemisphere (using mask selections)
+                    p = np.poly1d(np.polyfit(bandmu[mask], bandc[mask],4))
+                    # Define a linear space to show the polynomial adjustment variation over all emission angle range
+                    t = np.linspace(mumin, 0.9, 100)
+                    # Some control printing
+                    print(f"{adj_location} polynome")
+                    print(p)
+                    print(bandc[mask])
+                    # Correct data on the slected latitude band
+                    cdata=bandc[mask]*p(1)/p(bandmu[mask])
+                    # Plot figure showing limb correction using polynomial adjustment method
+                    ax1 = plt.subplot2grid((1, 3), (0, 0))
+                    ax1.scatter(bandmu[mask], bandc[mask])
+                    ax1.plot(t, p(t), '-',color='red')
+                    ax2 = plt.subplot2grid((1, 3), (0, 1))
+                    ax2.plot(t, (p(1))/p(t), '-',color='red')
+                    ax3 = plt.subplot2grid((1, 3), (0, 2))
+                    ax3.scatter(bandmu[mask], cdata)
             # Save figure showing limb correction using polynomial adjustment method 
             filt = Wavenumbers(ifilt)
-            plt.savefig(f"{directory}{filt}_polynomial_adjustment.png", dpi=900)
-            plt.savefig(f"{directory}{filt}_polynomial_adjustment.eps", dpi=900)
+            plt.savefig(f"{directory}{filt}_polynomial_adjustment_{adj_location}.png", dpi=900)
+            plt.savefig(f"{directory}{filt}_polynomial_adjustment_{adj_location}.eps", dpi=900)
 
             # Apply polynomial adjustment over individual cmaps depending of wave value
             for ifile, iwave in enumerate(wavenumber):
                 if iwave == wave:
-                    if viewing_mode[ifile] == 1:
+                    if adj_location == 'hemispheric':
+                        if viewing_mode[ifile] == 1:
+                            if (ifilt != 6): # ifilt = 6 is only a southern view
+                                cmaps[ifile, :, :] = cmaps[ifile, :, :] * p_north(1) / p_north(mumaps[ifile, :, :])
+                        if viewing_mode[ifile] == -1:
+                            cmaps[ifile, :, :] = cmaps[ifile, :, :] * p_south(1) / p_south(mumaps[ifile, :, :])
+                    elif adj_location == 'northern':
                         if (ifilt != 6): # ifilt = 6 is only a southern view
-                            cmaps[ifile, :, :] = cmaps[ifile, :, :] * p_north(1) / p_north(mumaps[ifile, :, :])
-                    if viewing_mode[ifile] == -1:
-                        cmaps[ifile, :, :] = cmaps[ifile, :, :] * p_south(1) / p_south(mumaps[ifile, :, :])
+                            cmaps[ifile, :, :] = cmaps[ifile, :, :] * p(1) / p(mumaps[ifile, :, :])
+                    else: # adj_location == 'southern' or adj_location == 'average':
+                        cmaps[ifile, :, :] = cmaps[ifile, :, :] * p(1) / p(mumaps[ifile, :, :])
+                        
 
         # Clear figure to avoid overlapping between plotting subroutines
         plt.clf()
 
-    return cmaps, mumaps, wavenumber 
+    return cmaps, mumaps, wavenumber, adj_location 
