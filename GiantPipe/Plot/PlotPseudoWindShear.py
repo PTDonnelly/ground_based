3d14cce0c52cff5fc27399cb1634558d60d650a3
Deborah Bardet <43414461+debbardet@users.noreply.github.com>
2022-07-12 16:36:08 +0100

    Correction in PlotPseudoWindShear subroutine

--- GiantPipe/Plot/PlotPseudoWindShear.py
+++ GiantPipe/Plot/PlotPseudoWindShear.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import Globals
 from Tools.VisirFilterInfo import Wavenumbers
 from Read.ReadZonalWind import ReadZonalWind
+from Read.ReadGravity import ReadGravity
 
 
-def PlotPseudoWindShear(windshear):
-    #  Load Jupiter zonal jets data to determine belts and zones location
-    ejets_c, wjets_c, nejet, nwjet = ReadZonalWind("../inputs/2003porco_zonalu.dat")
+def PlotPseudoWindShear(globalmaps, adj_location):
+    
 
     print('Plotting pseudo wind shear...')
     # If subdirectory does not exist, create it
@@ -16,9 +16,22 @@ def PlotPseudoWindShear(windshear):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-    for ifilt in range(Globals.nfilters):
-        lat=Globals.latgrid
+    # Initialize some local variales
+    ny = 360
+    lat = np.arange(-89.75,90,step=0.5)               # Latitude range from pole-to-pole
+    zonalmean = np.empty((Globals.nfilters, ny))
+    windshear = np.empty((Globals.nfilters, ny))
+    #  Load Jupiter zonal jets data to determine belts and zones location
+    ejets_c, wjets_c, nejet, nwjet = ReadZonalWind("../inputs/jupiter_jets.dat")
+    # Load Jupiter gravity data to calculate pseudo-windshear using TB and mu array array
+    grav, Coriolis, y, _, _, _ = ReadGravity("../inputs/jup_grav.dat", lat=lat)
 
+    for ifilt in range(Globals.nfilters):
+        # Zonal mean of the gloal maps
+        for iy in range(ny):
+            zonalmean[ifilt, iy] = np.nanmean(globalmaps[ifilt, iy, :])
+        # Calculated the associated thermal/pseudo-windshear
+        windshear[ifilt,:]=-(grav/(Coriolis*zonalmean[ifilt,:]))*np.gradient(zonalmean[ifilt, :],y)
         # Subplot for the southern hemisphere
         ax1 = plt.subplot2grid((1, 2), (0, 0))
         latkeep = (lat <-5)
@@ -34,8 +47,7 @@ def PlotPseudoWindShear(windshear):
         ax1.plot([-90,-10],[0,0],linewidth=1.0,color="grey")
         ax1.set_ylim([-0.7,0.7])
         ax1.set_xlim(-90,-10)
-        ax1.xlabel('Planetocentric Latitude',size=25)
-        ax1.ylabel('du/dz [m s$^{-1}$ km$^{-1}$]',size=25)
+        
 
         # Subplot for the northern hemisphere
         ax2 = plt.subplot2grid((1, 2), (0, 1))
@@ -52,9 +64,9 @@ def PlotPseudoWindShear(windshear):
         for iwjet in range(0,nwjet):
             ax1.plot([wjets_c[iwjet],wjets_c[iwjet]],[-15,15],color='black',linestyle="dotted")
         ax2.plot([10,90],[0,0],linewidth=1.0,color="grey")
-        ax2.xlabel('Planetocentric Latitude',size=25)
+        
 
         # Save figure showing calibation method 
         filt = Wavenumbers(ifilt)
-        plt.savefig(f"{dir}{filt}_pseudo_wind_shear.png", dpi=900)
-        plt.savefig(f"{dir}{filt}_pseudo_wind_shear.eps", dpi=900)
+        plt.savefig(f"{dir}{filt}_pseudo_wind_shear_{adj_location}_adj.png", dpi=900)
+        plt.savefig(f"{dir}{filt}_pseudo_wind_shear_{adj_location}_adj.eps", dpi=900)
