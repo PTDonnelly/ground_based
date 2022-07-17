bc268195fac62226409adad231061fe2f019d842
Deborah Bardet <43414461+debbardet@users.noreply.github.com>
2022-07-17 16:43:14 +0100

    Creation of a composite subroutine plotting to be able to mixte southern (for QBand filters) and average (NBand filters) adjustment results into the composite figure in PlotPseudoWindShear

--- GiantPipe/Plot/PlotPseudoWindShear.py
+++ GiantPipe/Plot/PlotPseudoWindShear.py
@@ -2,13 +2,14 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import Globals
+from Tools.SetWave import SetWave
 from Tools.VisirFilterInfo import Wavenumbers
 from Read.ReadZonalWind import ReadZonalWind
 from Read.ReadGravity import ReadGravity
 
 
 def PlotPseudoWindShear(globalmaps, adj_location):
-    
+    """ Plotting thermal shear using calculated global maps array """
 
     print('Plotting pseudo wind shear...')
     # If subdirectory does not exist, create it
@@ -72,14 +73,46 @@ def PlotPseudoWindShear(globalmaps, adj_location):
         plt.xlabel("Latitude", size=15)
         plt.ylabel("Pseudo-shear (m s$^{-1}$ km$^{-1}$)", size=15)
         # Save figure
-        plt.savefig(f"{dir}{filt}_pseudo_wind_shear_{adj_location}_adj.png", dpi=300)
-        plt.savefig(f"{dir}{filt}_pseudo_wind_shear_{adj_location}_adj.eps", dpi=300)
+        plt.savefig(f"{dir}calib_{filt}_pseudo_wind_shear_{adj_location}_adj.png", dpi=300)
+        plt.savefig(f"{dir}calib_{filt}_pseudo_wind_shear_{adj_location}_adj.eps", dpi=300)
 
+
+def PlotCompositePseudoWindShear():
+    """ Plotting thermal shear using stored global maps numpy array """
+
+    print('Plotting composite figure of pseudo wind shear...')
+    # If subdirectory does not exist, create it
+    dir = '../outputs/pseudo_wind_shear_profiles_figures/'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    # Initialize some local variales
+    nx, ny = 720, 360
+    lat = np.arange(-89.75,90,step=0.5)               # Latitude range from pole-to-pole
+    globalmaps = np.empty((Globals.nfilters, ny, nx))
+    zonalmean = np.empty((Globals.nfilters, ny))
+    windshear = np.empty((Globals.nfilters, ny))
+    #  Load Jupiter zonal jets data to determine belts and zones location
+    ejets_c, wjets_c, nejet, nwjet = ReadZonalWind("../inputs/jupiter_jets.dat")
+    # Load Jupiter gravity data to calculate pseudo-windshear using TB and mu array array
+    grav, Coriolis, y, _, _, _ = ReadGravity("../inputs/jup_grav.dat", lat=lat)
+
+    for ifilt in range(Globals.nfilters):
+        filt = Wavenumbers(ifilt)
+        adj_location = 'average' if ifilt < 10 else 'southern'
+        globalmaps[ifilt, :, :] = np.load(f'../outputs/global_maps_figures/calib_{filt}_global_maps_{adj_location}_adj.npy')
+        # Zonal mean of the global maps
+        for iy in range(ny):
+            zonalmean[ifilt, iy] = np.nanmean(globalmaps[ifilt, iy, :])
+        # Calculated the associated thermal/pseudo-windshear
+        windshear[ifilt,:]=-(grav/(Coriolis*zonalmean[ifilt,:]))*np.gradient(zonalmean[ifilt, :],y)
+    
     # Create a composite figure with all filters
     fig, axes = plt.subplots(Globals.nfilters, 2, figsize=(12,20), sharey=True)
     iaxes = 0
     for ifilt in [0,10,11,12,5,4,6,7,8,9,3,2,1]:
         filt = Wavenumbers(ifilt)
+        wavelength, _, _, _ = SetWave(_, filt)
         latkeep = (lat <-5)
         axes[iaxes,0].plot(lat[latkeep],windshear[iaxes,latkeep],linewidth=3.0,color="black")
         negkeep = (lat <-5) & (windshear[iaxes,:] < 0)
@@ -95,7 +128,7 @@ def PlotPseudoWindShear(globalmaps, adj_location):
         axes[iaxes,0].set_ylim(-0.7,0.7)
         # Subplot for the northern hemisphere
         latkeep = (lat > 5)       
-        axes[iaxes,1].plot(lat[latkeep],windshear[iaxes,latkeep],linewidth=3.0,color="black",label=f"{filt}"+" (cm$^{-1}$)")
+        axes[iaxes,1].plot(lat[latkeep],windshear[iaxes,latkeep],linewidth=3.0,color="black",label=f"{wavelength}"+"$\mu$m")
         negkeep = (lat > 5) & (windshear[iaxes,:] < 0)
         axes[iaxes,1].plot(lat[negkeep],windshear[iaxes,negkeep],"bo")
         poskeep = (lat > 5) & (windshear[iaxes,:] > 0)
@@ -116,5 +149,5 @@ def PlotPseudoWindShear(globalmaps, adj_location):
         plt.ylabel("Pseudo-shear (m s$^{-1}$ km$^{-1}$)", size=25)
         iaxes += 1
     # Save figure 
-    plt.savefig(f"{dir}pseudo_wind_shear_{adj_location}_adj.png", dpi=300)
-    plt.savefig(f"{dir}pseudo_wind_shear_{adj_location}_adj.eps", dpi=300)
+    plt.savefig(f"{dir}calib_pseudo_wind_shear.png", dpi=300)
+    plt.savefig(f"{dir}calib_pseudo_wind_shear.eps", dpi=300)
