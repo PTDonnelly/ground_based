9d46e2f2a79465ba595c99d8175623eacae89623
Deborah Bardet <43414461+debbardet@users.noreply.github.com>
2022-08-01 15:39:35 +0100

    Adding nx and ny in Globals as are used everywhere in the pipeline

--- GiantPipe/Globals.py
+++ GiantPipe/Globals.py
@@ -3,11 +3,12 @@
 
 import numpy as np
 
+nx, ny = 720, 360                               # Dimensions of an individual cylindrical map
 latrange    = -90, 90          			         # Latitude range for binning pixels (planetographic)
-latstep     = 1                                  # Latitude increment for binning pixels (planetographic)
-latgrid     = np.arange(-89.5, 90, latstep)      # Latitude range from pole-to-pole
+latstep     = 1                                 # Latitude increment for binning pixels (planetographic)
+latgrid     = np.arange(-89.5, 90, latstep)     # Latitude range from pole-to-pole
 nlatbins    = len(latgrid)
-lonrange    = 360, 0            		         # Longitude range for binning pixels (Sys III)
-merid_width = 30                                 # Longitude range about the central meridian for averaging
-nfilters    = 13                                 # Set manually if using irregularly-sampled data
-# mu_max      = 80.0                 		     # Maximum emission angle
+lonrange    = 360, 0            		            # Longitude range for binning pixels (Sys III)
+merid_width = 30                                # Longitude range about the central meridian for averaging
+nfilters    = 13                                # Set manually if using irregularly-sampled data
+# mu_max      = 80.0                 		      # Maximum emission angle
